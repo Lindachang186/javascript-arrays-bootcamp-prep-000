cb4cf601f8e446b5b6bd45ccdfaa91369fda71f1
Linda Chang <lindachang186@gmail.com>
2018-06-19 15:06:51 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(chocolateBars, "hersheys") {
+  chocolateBars = ['hersheys',...chocolateBars];
+}
